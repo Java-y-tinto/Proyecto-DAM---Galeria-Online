026b12bcb680fc68a7a0c592c116cf603a4fccaf
Java-y-tinto <jromrue1904@g.educaand.es>
2025-05-30 20:04:11 +0200

    pene

--- middleware/scripts/setup-odoo-ci.js
+++ middleware/scripts/setup-odoo-ci.js
@@ -218,7 +218,7 @@ async function main() {
 
     await checkOdooAvailability();
     const client = await connectToOdoo();
-   // await setupTestData(client);
+    await setupTestData(client);
    await createOrUpdateField(client);
     await simpleAuthenticatedCall(client);
     console.log('🎉 Configuración de datos de prueba completada exitosamente!');
